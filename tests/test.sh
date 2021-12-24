dfb08629954d4e1e8408d3deb72d737d6b74f7ef
Mikhail Sokolovskiy <sokolmish@gmail.com>
2021-12-24 20:59:09 +0300

    Simplify tests launcher

--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-../cmake-build-debug/cw39 ./tst_prog1.c > ./tst1.ll &&
-  llvm-as-13 ./tst1.ll && lli-13 ./tst1.bc
+../cmake-build-debug/cw39 ./tst_prog1.c > ./tst1.ll && lli-13 ./tst1.ll
