a9e99a406c858b23fd5c32d9c17c4e9eec90d587
Mikhail Sokolovskiy <sokolmish@gmail.com>
2021-12-27 00:27:47 +0300

    Move IR_value class into separate files

--- src/ir/nodes.hpp
+++ src/ir/nodes.hpp
@@ -5,68 +5,13 @@
 #include <map>
 #include <memory>
 #include <optional>
-#include <variant>
 #include <string>
+
 #include "types.hpp"
+#include "value.hpp"
+
 #include "parser/common.hpp"
 
-// Values
-
-class IRval {
-public:
-    typedef std::variant<
-            uint8_t, int8_t, uint32_t, int32_t, uint64_t, int64_t,
-            float, double> union_type;
-    enum ValueClass { VAL, VREG, GLOBAL, STRING, FUN_PARAM } valClass;
-
-private:
-    std::shared_ptr<IR_Type> type;
-    union_type val;
-
-public:
-    std::optional<int> version = {};
-
-    IRval(std::shared_ptr<IR_Type> type, ValueClass vclass, union_type v);
-    IRval copy() const;
-
-    // TODO: clear versioned and unversioned comparsion
-    bool operator==(IRval const &oth) const;
-
-    static bool less(const IRval &a, const IRval &b);
-    struct Comparator {
-        bool operator()(const IRval& a, const IRval& b) const;
-    };
-
-    static bool lessVersions(const IRval &a, const IRval &b);
-    struct ComparatorVersions {
-        bool operator()(const IRval& a, const IRval& b) const;
-    };
-
-    [[nodiscard]] static IRval createVal(std::shared_ptr<IR_Type> type, union_type v);
-    [[nodiscard]] static IRval createReg(std::shared_ptr<IR_Type> type, uint64_t id);
-    [[nodiscard]] static IRval createFunArg(std::shared_ptr<IR_Type> type, uint64_t num);
-    [[nodiscard]] static IRval createString(uint64_t num);
-    [[nodiscard]] static IRval createGlobal(std::shared_ptr<IR_Type> globalType, uint64_t num);
-    [[nodiscard]] static IRval createDefault(std::shared_ptr<IR_Type> type);
-
-    std::shared_ptr<IR_Type> const& getType() const;
-    bool isConstant() const;
-    bool isVReg() const;
-    bool isGlobal() const;
-    union_type const& getVal() const;
-    std::string to_string() const;
-    std::string to_reg_name() const;
-
-    template <class T>
-    T castValTo() const {
-        return std::visit([](auto const &arg) -> T {
-            return static_cast<T>(arg);
-        }, val);
-    }
-};
-
-
-// Expressions
 
 struct IR_ExprOper;
 struct IR_ExprAlloc;
