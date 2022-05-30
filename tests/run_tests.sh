82a8120408d0ecfe9ef65f60284c109cf110ff2a
Mikhail Sokolovskiy <sokolmish@gmail.com>
2022-05-30 22:20:37 +0300

    Update S1 test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -60,6 +60,6 @@ run_testcase ./tst_prog4.c
 run_testcase ./tst_prog5.c
 run_testcase ./tst_prog6.c
 run_testcase ./tst_prog7.c
-run_testcase ./s1_test.c
+run_testcase ./s1_test.c 800
 
 echo -e "[${green}PASSED${nc}] ${bold}All tests finished successfully${nb}"
--- tests/s1_test.c
+++ tests/s1_test.c
@@ -3,16 +3,13 @@ int atoi(const char *nptr);
 
 typedef unsigned int u32;
 
-//int count_zeroes1(u32 mask) {
-//    int cnt = 0;
-//    while ((mask & 1) == 0) {
-//        mask >>= 1;
-//        ++cnt;
-//    }
-//    return cnt;
-//}
+__attribute__((noinline))
+void stop_dce(u32 *addr, u32 x) {
+    volatile u32 *vaddr = addr;
+    *vaddr = x;
+}
 
-int count_zeroes2(u32 mask) {
+static inline int count_zeroes2(u32 mask) {
     u32 cnt = 0;
     while (((mask >> cnt) & 1) == 0) {
         ++cnt;
@@ -20,23 +17,22 @@ int count_zeroes2(u32 mask) {
     return cnt;
 }
 
-int main() {
-    u32 arg = atoi("800");
-    int x1 = 0, x2 = 0, x3 = 0;
-    x1 = __builtin_ctz(arg);
-//    x2 = count_zeroes1(arg);
-    x3 = count_zeroes2(arg);
-    printf("%d %d %d\n", x1, x2, x3);
+int main(int argc, char **argv) {
+    if (argc < 2)
+        return 0;
+    u32 arg = atoi(argv[1]);
+    u32 space;
+    for (int i = 0; i < 5000000; i++) { // 1000000000
+        int x1 = 0, x3 = 0;
+        if (i % 2 == 0)
+            arg += 4;
+        else
+            arg -= 4;
+        x1 = __builtin_ctz(arg);
+        x3 = count_zeroes2(arg);
+//        printf("%d %d\n", x1, x3);
+        stop_dce(&space, x1);
+        stop_dce(&space, x3);
+    }
     return 0;
 }
-
-//int count_zeroes3(u32 mask) {
-//    u32 cnt = 0;
-//    int x = 0;
-//    while (((mask >> cnt) & 1) == 0) {
-//        ++cnt;
-//        x++;
-//    }
-//    printf("%d\n", x);
-//    return cnt;
-//}
