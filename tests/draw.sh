16b08e30c878fd8d4519e0f304794ce87459f58c
Mikhail Sokolovskiy <sokolmish@gmail.com>
2022-06-20 22:11:35 +0300

    Remove some old files

--- tests/draw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-inFile="$1"
-ext="svg"
-
-[ -z "$inFile" ] && inFile="tst_prog3.c"
-# && echo "No file" >&2 && exit 1
-
-../cmake-build-debug/cw39 --cfg "$inFile" | dot -T${ext} -o "graph.${ext}"
